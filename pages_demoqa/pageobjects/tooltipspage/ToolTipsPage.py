dee733adb71d8f0fc1e0a99400246b77ca182b11
Denes Gombis <gd9603@gmail.com>
2021-05-27 15:23:56 +0200

    Add WidgetTests

---
+++ pages_demoqa/pageobjects/tooltipspage/ToolTipsPage.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote import webelement
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+from pages_base.pageobjects.AbstractPage import AbstractPage
+from utils.enums.BrowserEnum import BrowserEnum
+
+
+class ToolTipsPage(AbstractPage):
+    IMPLICIT_WAIT = 5
+    PAUSE = 0.5;
+
+    toolTipButtonLocator = '#toolTipButton'
+    toolTipButton = None
+
+    toolTipTextFieldLocator = '#toolTipTextField'
+    toolTipTextField = None
+
+    toolTipTextLocator = '.tooltip-inner'
+    toolTipText = None
+
+    def __init__(self, driver: webdriver, browser: BrowserEnum):
+        super().__init__(driver, browser)
+        self.toolTipButton = driver.find_element(By.CSS_SELECTOR, self.toolTipButtonLocator)
+        self.toolTipTextField = driver.find_element(By.CSS_SELECTOR, self.toolTipTextFieldLocator)
+
+    def hover_tool_tip_button_and_get_text(self):
+        return self.hover_web_element_get_tool_tip_text(self.toolTipButton)
+
+    def hover_tool_tip_text_field_and_get_text(self):
+        return self.hover_web_element_get_tool_tip_text(self.toolTipTextField)
+
+    def hover_web_element_get_tool_tip_text(self, web_element: webelement):
+        action = ActionChains(self.driver)
+        action.pause(self.PAUSE).perform()
+        action.move_to_element(web_element).perform()
+        action.pause(self.PAUSE).perform()
+
+        element = WebDriverWait(self.driver, self.IMPLICIT_WAIT).until(
+            expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, self.toolTipTextLocator)))
+        return element.text
+
+
--- pages_demoqa/pageobjects/widgetpage/WidgetPage.py
+++ pages_demoqa/pageobjects/widgetpage/WidgetPage.py
@@ -1,19 +1,31 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 from pages_base.pageobjects.AbstractPage import AbstractPage
+from pages_demoqa.pageobjects.tooltipspage.ToolTipsPage import ToolTipsPage
 from utils.enums.BrowserEnum import BrowserEnum
 
 
 class WidgetPage(AbstractPage):
-    IMPLICIT_WAIT = 2
+    IMPLICIT_WAIT = 10
 
-    uploadFileLocator = '#uploadFile'
-    uploadFile = None
+    toolTipsMenuItemLocator = '//span[text()="Tool Tips"]'
+    toolTipsMenuItem = None
 
-    filePathLocator = '#uploadedFilePath'
-    filePath: None
+    bodyLocator = 'body'
+    body = None
 
     def __init__(self, driver: webdriver, browser: BrowserEnum):
         super().__init__(driver, browser)
-        self.uploadFile = driver.find_element(By.CSS_SELECTOR, self.uploadFileLocator)
+        self.body = driver.find_element(By.CSS_SELECTOR, self.bodyLocator)
+
+    def navigate_to_tool_tips_page(self):
+        self.body.send_keys(Keys.PAGE_DOWN)
+        WebDriverWait(self.driver, self.IMPLICIT_WAIT).until(
+            expected_conditions.element_to_be_clickable((By.XPATH, self.toolTipsMenuItemLocator))).click()
+        return ToolTipsPage(self.driver, self.browser)
